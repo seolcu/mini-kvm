d53dfacad214f24323cf32452607eaf41ba135b4
Gyuwon Seol <seolcu0112@proton.me>
2025-10-14 12:12:10 +0900

    remove obsolete code

--- my-hypervisor/target/riscv64gc-unknown-none-elf/debug/deps/my_hypervisor-fd4ba64e4f1f0236.d
+++
@@ -1,8 +0,0 @@
-/home/seolcu/문서/코드/mini-kvm/my-hypervisor/target/riscv64gc-unknown-none-elf/debug/deps/my_hypervisor-fd4ba64e4f1f0236.d: src/main.rs src/print.rs src/trap.rs src/allocator.rs
-
-/home/seolcu/문서/코드/mini-kvm/my-hypervisor/target/riscv64gc-unknown-none-elf/debug/deps/my_hypervisor-fd4ba64e4f1f0236: src/main.rs src/print.rs src/trap.rs src/allocator.rs
-
-src/main.rs:
-src/print.rs:
-src/trap.rs:
-src/allocator.rs:
--- my-hypervisor/target/riscv64gc-unknown-none-elf/debug/deps/scopeguard-25ff676b0fea63d5.d
+++
@@ -1,7 +0,0 @@
-/home/seolcu/문서/코드/mini-kvm/my-hypervisor/target/riscv64gc-unknown-none-elf/debug/deps/scopeguard-25ff676b0fea63d5.d: /home/seolcu/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/scopeguard-1.2.0/src/lib.rs
-
-/home/seolcu/문서/코드/mini-kvm/my-hypervisor/target/riscv64gc-unknown-none-elf/debug/deps/libscopeguard-25ff676b0fea63d5.rlib: /home/seolcu/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/scopeguard-1.2.0/src/lib.rs
-
-/home/seolcu/문서/코드/mini-kvm/my-hypervisor/target/riscv64gc-unknown-none-elf/debug/deps/libscopeguard-25ff676b0fea63d5.rmeta: /home/seolcu/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/scopeguard-1.2.0/src/lib.rs
-
-/home/seolcu/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/scopeguard-1.2.0/src/lib.rs:
